30ab4493b66d2d09c093df5c43cf09c038cfc97c
steve Manangu <stevenmanangu360@gmail.com>
2022-09-18 11:05:30 +0300

    created a hook for the input form

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -3,11 +3,15 @@ import React, {Fragment, useState} from 'react';
 const InputTodo = () => {
     // setup react hooks to grab the inputs state
     const [description, setDescription] = useState("");
+
     return(
         <Fragment>
             <h1 className="text-center mt-5">My amazing Todo List</h1>
             <form className="d-flex mt-5">
-                <input type="text" className="form-control"/>
+                <input type="text" className="form-control" 
+                value={description}
+                onChange={e => setDescription(e.target.value)}
+                />
                 <button className="btn btn-success">Add</button>
             </form>
         </Fragment>
