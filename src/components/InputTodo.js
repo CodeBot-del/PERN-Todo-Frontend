5d28241378eb7e081aac783eaee73d30d2237cc6
steve Manangu <stevenmanangu360@gmail.com>
2022-09-18 10:56:21 +0300

    input component lookingn good

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, {Fragment, useState} from 'react';
 
 const InputTodo = () => {
+    // setup react hooks to grab the inputs state
+    const [description, setDescription] = useState("");
     return(
-        <h1>Input Todo</h1>
+        <Fragment>
+            <h1 className="text-center mt-5">My amazing Todo List</h1>
+            <form className="d-flex mt-5">
+                <input type="text" className="form-control"/>
+                <button className="btn btn-success">Add</button>
+            </form>
+        </Fragment>
     )
 };
+
+export default InputTodo;
