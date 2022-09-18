b127ad4d793e5c4171bf3bcca1c33df4bb48325e
steve Manangu <stevenmanangu360@gmail.com>
2022-09-18 23:49:33 +0300

    added the delete funcion

--- src/components/ListTodos.js
+++ src/components/ListTodos.js
@@ -1,6 +1,38 @@
 import React, {Fragment, useState, useEffect} from 'react';
+import EditTodo from "./EditTodo";
 
 const ListTodos = () => {
+    const [todos, setTodos] = useState([]);
+
+    // delete todo function
+    const deleteTodo = async (id) => {
+        try {
+            const deleteTodo = await fetch(`http://localhost:5000/todos/${id}`, {
+                method: "DELETE",
+            });
+
+            setTodos(todos.filter(todo => todo.todo_id !== id))
+        } catch (err) {
+            console.error(err.message);
+        }
+    }
+    
+    const getTodos = async() => {
+        try {
+            const response = await fetch("http://localhost:5000/todos");
+            const jsonData = await response.json();
+
+
+            setTodos(jsonData);
+        } catch (err) {
+            console.error(err.message);
+        }
+    }
+    // a useEffect will be fetching a restful API every time the page is refreshed.
+    useEffect(() => {
+        getTodos();
+    },[]);
+
     return(
         <Fragment>
             <table class="table table-hover mt-5 text-center">
@@ -19,7 +51,20 @@ const ListTodos = () => {
                     <td>john@example.com</td>
                 </tr>
                     */}
-                
+                {todos.map(todo => (
+                    <tr key={todo.todo_id}>
+                        <td>{todo.description}</td>
+                        <td>
+                            <EditTodo/>
+                        </td>
+                        <td><button className="btn btn-danger"
+                        onClick={() => deleteTodo(todo.todo_id)}
+                        >
+                        Delete
+                        </button>
+                        </td>
+                    </tr>
+                ))}
                 
                 </tbody>
             </table>
