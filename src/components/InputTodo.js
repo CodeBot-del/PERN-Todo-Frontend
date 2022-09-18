c1c8db351ecf2efe20fc07ecedf6248be64a0b6d
steve Manangu <stevenmanangu360@gmail.com>
2022-09-18 11:20:16 +0300

    successfully inserted new todos to the database server

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -4,10 +4,27 @@ const InputTodo = () => {
     // setup react hooks to grab the inputs state
     const [description, setDescription] = useState("");
 
+    const onSubmitForm = async(e) => {
+        e.preventDefault();  //prevent the form from refreshing
+        try {
+            const body = {description};
+            const response = await fetch("http://localhost:5000/todos", {
+                method: "POST", //we making a post request
+                headers: {"Content-Type": "application/json"}, //contents are sent in json format.
+                body: JSON.stringify(body) //convert the body to a string
+            });
+
+            
+            console.log(response);
+        } catch (err) {
+            console.error(err.message);
+        }
+    }
+
     return(
         <Fragment>
             <h1 className="text-center mt-5">My amazing Todo List</h1>
-            <form className="d-flex mt-5">
+            <form className="d-flex mt-5" onSubmit={onSubmitForm}>
                 <input type="text" className="form-control" 
                 value={description}
                 onChange={e => setDescription(e.target.value)}
