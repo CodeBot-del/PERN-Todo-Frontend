21132805d2d55f819a62e7801ac0583c5c5741f1
steve Manangu <stevenmanangu360@gmail.com>
2022-09-19 00:18:05 +0300

    struggling with edit todos

--- src/components/EditTodo.js
+++ src/components/EditTodo.js
@@ -1,36 +1,38 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useState} from 'react';
 
-const EditTodo = () => {
+const EditTodo = ({todo}) => {
+    const [description, setDescription] = useState(todo.description);
     return <Fragment>
         {/* <!-- Button to Open the Modal --> */}
-<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">
-  Open modal
-</button>
+        <button type="button" class="btn btn-warning" data-toggle="modal" data-target={`#id${todo.todo_id}`}>
+        Edit
+        </button>
 
-{/* <!-- The Modal --> */}
-<div class="modal" id="myModal">
-  <div class="modal-dialog">
-    <div class="modal-content">
+        {/* <!-- The Modal --> */}
+        <div class="modal" id={`id${todo.todo_id}`}>
+        <div class="modal-dialog">
+            <div class="modal-content">
 
-      {/* <!-- Modal Header --> */}
-      <div class="modal-header">
-        <h4 class="modal-title">Modal Heading</h4>
-        <button type="button" class="close" data-dismiss="modal">&times;</button>
-      </div>
+            {/* <!-- Modal Header --> */}
+            <div class="modal-header">
+                <h4 class="modal-title">Edit Todo</h4>
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+            </div>
 
-      {/* <!-- Modal body --> */}
-      <div class="modal-body">
-        Modal body..
-      </div>
+            {/* <!-- Modal body --> */}
+            <div class="modal-body">
+                <input type="text" className="form-control" value={description}/>
+            </div>
 
-      {/* <!-- Modal footer --> */}
-      <div class="modal-footer">
-        <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
-      </div>
+            {/* <!-- Modal footer --> */}
+            <div class="modal-footer">
+                <button type="button" class="btn btn-warning" data-dismiss="modal">Edit</button>
+                <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+            </div>
 
-    </div>
-  </div>
-</div>
+            </div>
+        </div>
+        </div>
     </Fragment>;
 }
 
